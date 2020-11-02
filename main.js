df05f1520f2664807f408a28741e3265c39cbfea
Techabdull <deltechaub@gmail.com>
2020-11-02 13:57:27 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.body').height($(window).height());
+  })
+  justify-content: center;
+display: flex;
+padding: 0%;
+margin: 0% auto;
+margin-top: -1.30%;
+width: 70%;
+max-height: 10%;
